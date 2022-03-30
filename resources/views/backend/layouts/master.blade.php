0f26fe2758be5205e440549568c76cd00274513e
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-03-30 22:29:45 +0600

    mastering end

---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield('title','Dashboard | Role Permission Laravel')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @include("backend.layouts.partials.style")
+    <!-- modernizr css -->
+    <script src="{{ asset('backend/assets/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+</head>
+
+<body>
+
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="{{ asset('backend/assets/images/icon/logo.png') }}" alt="logo"></a>
+                </div>
+            </div>
+            @include("backend.layouts.partials.menu")
+        </div>
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+            @include("backend.layouts.partials.header")
+
+            @yield('content')
+
+        </div>
+        <!-- main content area end -->
+        <!-- footer area start-->
+       @include("backend.layouts.partials.footer")
+        <!-- footer area end-->
+    </div>
+    <!-- page container area end -->
+    <!-- offset area start -->
+   @include("backend.layouts.partials.offset")
+    <!-- offset area end -->
+    <!-- jquery latest version -->
+    @include("backend.layouts.partials.scripts")
+</body>
+
+</html>
