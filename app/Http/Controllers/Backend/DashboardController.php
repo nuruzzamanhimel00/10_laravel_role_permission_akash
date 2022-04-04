6451a99654262ea4874b4a8c43a107b8f6258c31
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-04 19:32:14 +0600

    done

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -7,11 +7,19 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public $user;
+
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware(function($request,$next){
+            $this->user = $this->guard()->user();
+            return $next($request);
+        });
     }
     public function index(){
+        if( is_null($this->user) || !$this->user->can('dashboard.view') ){
+            abort(403, 'Unauthonticated Access');
+        }
         return view("backend.pages.dashboard.index");
         // return view('backend.layouts.master');
     }
